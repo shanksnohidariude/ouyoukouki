439e87901751c342bb5b26283bcf91491d9f1c5f
Ryo Nakamura(中村 凌) <149981734+shanksnohidariude@users.noreply.github.com>
2025-10-06 04:39:01 +0000

    hello

--- spring-boot-app/src/main/java/com/example/spring_boot_app/AuthController.java
+++ spring-boot-app/src/main/java/com/example/spring_boot_app/AuthController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RestController
@@ -39,6 +41,42 @@ public class AuthController {
     public ResponseEntity<Map<String, Object>> authUser(@RequestHeader("Authorization") String authorizationHeader) {
         Map<String, Object> user = supabaseAuthService.getUserByAccessToken(authorizationHeader.substring(7));
         return ResponseEntity.ok(Map.of("email", user.get("email")));
+    }
+
+    /**
+     * ログインを行います
+     * @param request アカウント情報
+     * @return 実行結果
+     */
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody AuthRequest request) {
+        Map<String, Object> result = supabaseAuthService.loginWithPassword(request.getEmail(), request.getPassword());
+        return result.containsKey("access_token")
+                ? ResponseEntity.ok(result)
+                : ResponseEntity.badRequest().body(result);
+    }
+
+    /**
+     * ログアウトを行います
+     * @param authorizationHeader Authorizationヘッダ
+     * @return 実行結果
+     */
+    @PostMapping("/logout")
+    public ResponseEntity<Map<String, Object>> logout(@RequestHeader("Authorization") String authorizationHeader) {
+        supabaseAuthService.logout(authorizationHeader.substring(7));
+        return ResponseEntity.ok(Map.of("message", "Logout successful."));
+    }
+
+    /**
+     * Github認証にリダイレクトします
+     * @param response HTTPレスポンス
+     * @param uriBuilder URI構築
+     */
+    @GetMapping("/oauth2/github")
+    public void redirectToGitHub(HttpServletResponse response, UriComponentsBuilder uriBuilder) throws IOException {
+        String redirectTo = uriBuilder.replacePath("/").build().toUriString();
+        String supabaseAuthGitHubUrl = supabaseAuthService.getGitHubSignInUrl(redirectTo);
+        response.sendRedirect(supabaseAuthGitHubUrl);
     }
 
 }
--- spring-boot-app/src/main/java/com/example/spring_boot_app/SupabaseAuthService.java
+++ spring-boot-app/src/main/java/com/example/spring_boot_app/SupabaseAuthService.java
@@ -5,6 +5,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Map;
 
 @Service
@@ -12,6 +13,9 @@ public class SupabaseAuthService {
 
     @Autowired
     private WebClient webClient;
+
+    @Value("${supabase.url}")
+    private String supabaseUrl;
     
     /**
      * Eメール/パスワードを使ってSupabase認証のアカウント登録を行います
@@ -31,6 +35,36 @@ public class SupabaseAuthService {
     }
 
     /**
+     * Eメール/パスワードを使ってSupabase認証を行います
+     * @param email Eメール
+     * @param password パスワード
+     * @return 認証結果
+     */
+    public Map<String, Object> loginWithPassword(String email, String password) {
+        return webClient.post()
+                .uri("/auth/v1/token?grant_type=password")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("email", email, "password", password))
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                .block();
+    }
+
+    /**
+     * アクセストークンよりログアウトを行います
+     * @param accessToken アクセストークン
+     */
+    public void logout(String accessToken) {
+        webClient.post()
+            .uri("/auth/v1/logout")
+            .header("Authorization", "Bearer " + accessToken)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+            .block();
+    }    
+    
+
+    /**
      * アクセストークンより Supabase のアカウント情報を取得する
      * @param accessToken アクセストークン
      * @return アカウント情報
@@ -43,5 +77,19 @@ public class SupabaseAuthService {
                 .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
                 .block();
     }
+
+    /**
+     * SupabaseのGitHub認証を開始するためのURLを取得する
+     * @param redirectTo アカウント認証時にコールバックするリダイレクトURL
+     * @return SupabaseのGitHub認証URL
+     */
+    public String getGitHubSignInUrl(String redirectTo) {
+        return UriComponentsBuilder.fromHttpUrl(this.supabaseUrl)
+                .path("/auth/v1/authorize")
+                .queryParam("provider", "github")
+                .queryParam("redirect_to", redirectTo)
+                .queryParam("scopes", "user:email")
+                .toUriString();
+    }    
 
 }
